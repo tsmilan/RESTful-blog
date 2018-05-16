6c1d863eec0eca265f486f0da0c1253cbd138db6
tsmilan <101035074@student.swin.edu.au>
2018-05-17 02:12:41 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// APP CONFIG
+app = express();
+mongoose.connect("mongodb://localhost/restfulblog");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// MONGOOSE MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {
+		type: Date, 
+		default: Date.now
+	}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// RESTFUL ROUTES
+
+app.get("/", function(req, res) {
+	res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res) {
+	Blog.find({}, function(err, blogs) {
+		if(err) {
+			console.log("ERROR");
+		} else {
+			res.render("index", {blogs: blogs});
+		}
+	})
+});
+
+app.listen(3000, "127.0.0.1", function() {
+	console.log("SERVER IS RUNNING");
+});
